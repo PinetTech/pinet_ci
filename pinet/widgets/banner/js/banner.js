b14c8b38f46d37689b5c31c4a7a980a4107e1294
Jack <guitarpoet@gmail.com>
2014-11-26 17:27:45 +0800

    Added the strategy support for banner.

--- pinet/widgets/banner/js/banner.js
+++ pinet/widgets/banner/js/banner.js
@@ -1,14 +1,28 @@
 $('.pinet_banner').each(function(){
 	var self = $(this);
 	var src = $(this).attr('src');
+	var strategy = self.attr('strategy');
 	var i = $(document.createElement('img'));
 	i.load(function(){
 		var img = $(this);
-		var height = img.height();
-		self.css({
-			'background': 'url(' + src + ') center center no-repeat',
-			'height': height
-		});
+		var css = {};
+		css.height = img.height();
+		css.background = 'url(' + src + ') center center no-repeat';
+
+		switch(strategy) {
+			case 'top':
+				css['background-size'] = '100%';
+				css.background = 'url(' + src + ') center top no-repeat';
+				break;
+			case 'bottom':
+				css['background-size'] = '100%';
+				css.background = 'url(' + src + ') center bottom no-repeat';
+				break;
+			case 'center':
+				css['background-size'] = '100%';
+				break;
+		}
+		self.css(css);
 		img.remove();
 	});
 	i.attr('src', src);
