0463dc471922525a05f10578d214ac3189f1309f
Jack <guitarpoet@gmail.com>
2014-11-26 18:11:00 +0800

    Removed the application.

--- application/language/chinese/admin_lang.php
+++
@@ -1,8 +0,0 @@
-<?php defined('BASEPATH') or exit('No direct script access allowed');
-
-$lang['ok'] = "提交";
-$lang['cancel'] = "取消";
-$lang['password']="邮箱";
-$lang['Password again']="密码";
-$lang['RESET']="登陆";
-
