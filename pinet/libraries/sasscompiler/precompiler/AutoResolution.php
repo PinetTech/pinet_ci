cd69a2b8ad71fbe88576b24bef0ce3e583d3fa91
andy <andy.ling@pinet.co>
2014-12-17 17:50:05 +0800

    Fixed auto resolution bug, Change tablelayout

--- pinet/libraries/sasscompiler/precompiler/AutoResolution.php
+++ pinet/libraries/sasscompiler/precompiler/AutoResolution.php
@@ -7,26 +7,47 @@
 
 			foreach($compiler->resolutions as $k => $r) {
 				$compiler->suffix .= '@media screen and (min-width: '.$r.'px){'."\n";
-				$compiler->suffix .= '$screen_width:'. $r.';';
-				$compiler->suffix .= '$alias_width:'.$k.';';
+				 if (is_numeric($k) && is_string($r) && !is_numeric($r) ) {
+				 	$compiler->suffix .= '$screen-width:'. $k.';';
+				 	$compiler->suffix .= '$alias-width:'.$r.';';
+				 	$screen_width = $k;
+				 	$alias_width =$r;
+				 }
+				 else if (is_numeric($k) && is_numeric($r)) {
+					$compiler->suffix .= '$screen-width:'. $r.';';
+				 	$compiler->suffix .= '$alias-width:0;';
+				 	$screen_width = $r;
+				 	$alias_width = 0;
+				 }
+				 else if (is_string($k) && !is_numeric($k)) {
+				 	$compiler->suffix .= '$screen-width:'. $r.';';
+				 	$compiler->suffix .= '$alias-width:'.$k.';';
+				 	$screen_width = $r;
+				 	$alias_width =$k;
+				 }
+				 else {
+				 	$compiler->suffix .= '$screen-width:'. $k.';';
+				 	$compiler->suffix .= '$alias-width:0;';
+				 	$screen_width = $k;
+				 	$alias_width =0;
+				 }
 				foreach($compiler->sasses as $s) {
 					$s = str_replace('.scss', '', $s);
 					$basename = basename($s);
 					$name = str_replace('/', '_', $s);
-					if($basename == $name) {
-						$this->addConstruct($basename, $compiler, $k.','.$r);
-					}else{
-						$this->addConstruct($name, $compiler, $k.','.$r);
+					if($basename != $name) {
+						$this->addConstruct($basename, $compiler, $screen_width.','.$alias_width);
 					}
+					$this->addConstruct($name, $compiler, $screen_width.','.$alias_width);
 				}
 				$compiler->suffix .= '}'."\n";
 			}
 		}
 
-		protected function addConstruct($name, $compiler, $r) {
+		protected function addConstruct($name, $compiler, $args) {
 			$the_name = 'responsive_'.$name;
 			if(strpos($compiler->content, $the_name) !== FALSE) {
-					$compiler->suffix .= "\t".'@include '.$the_name.'('.$r.'px);'."\n";
+					$compiler->suffix .= "\t".'@include '.$the_name.'('.$args.');'."\n";
 			}
 		}
 	}
