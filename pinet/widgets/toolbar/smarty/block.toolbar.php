3d1daab18368146aae0bf776e358f00c73ef21c0
andy <andy.ling@pinet.co>
2014-12-21 16:04:49 +0800

    Change all smarty_plugins to widgets

--- pinet/widgets/toolbar/smarty/block.toolbar.php
+++ pinet/widgets/toolbar/smarty/block.toolbar.php
@@ -4,6 +4,8 @@ if(!function_exists('smarty_block_container')) {
 	require_once(dirname(__FILE__).'/block.container.php');
 }
 
+
+
 function smarty_block_toolbar($params, $content = '', $template, &$repeat) {
 	if($repeat) { // This is the start part
 		add_container($params, $template, 'toolbar');
