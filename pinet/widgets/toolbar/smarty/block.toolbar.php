7f2aca144525655b7f3ec4f0af62e82c5ca81586
andy <andy.ling@pinet.co>
2014-12-21 18:12:48 +0800

    Change toolbar style

--- pinet/widgets/toolbar/smarty/block.toolbar.php
+++ pinet/widgets/toolbar/smarty/block.toolbar.php
@@ -4,8 +4,6 @@ if(!function_exists('smarty_block_container')) {
 	require_once(dirname(__FILE__).'/block.container.php');
 }
 
-
-
 function smarty_block_toolbar($params, $content = '', $template, &$repeat) {
 	if($repeat) { // This is the start part
 		add_container($params, $template, 'toolbar');
@@ -17,7 +15,7 @@ function smarty_block_toolbar($params, $content = '', $template, &$repeat) {
 		$classes = explode(' ', $classes);
 	}
 	$classes []= 'toolbar';
-	$classes []= 'pinet_toolbar';
+	$classes []= 'pinet-toolbar';
 	$params['class'] = implode(' ', $classes);
 	return build_tag('div', $params, $content);
 }
