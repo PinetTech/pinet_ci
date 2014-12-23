a1d59194276f981bcaae83f3809fb431090b8651
andy <andy.ling@pinet.co>
2014-12-23 16:25:34 +0800

    Change sass html function

--- pinet/widgets/form/js/pinet_form_init.js
+++ pinet/widgets/form/js/pinet_form_init.js
@@ -3,9 +3,13 @@ $(function(){
 		$("select").selectBoxIt();
 	}
 	if($.isFunction($.fn.jqBootstrapValidation)) {
-            	$("input,select,textarea").not("[type=image],[type=submit],[type=file]").jqBootstrapValidation();
-        	}
-        	if($.isFunction($.fn.inputmask)) {
-        		$("input[data-inputmask]").not("[type=image],[type=submit],[type=file]").inputmask().addClass('pinet-input-mask');
-        	}
+    	$("input,select,textarea").not("[type=image],[type=submit],[type=file]").jqBootstrapValidation();
+	}
+	if($.isFunction($.fn.inputmask)) {
+		$("input[data-inputmask]").not("[type=image],[type=submit],[type=file]").inputmask().addClass('pinet-input-mask');
+	}
+	if($.isFunction($.fn.pinet_cascadeSelect)) {
+		$('select').on('change', function() {
+		})
+	}
 });
