d2e6fe1a327bb8001063e3e6cb152f550ac19e82
lmjean <jean.lu@pinet.co>
2014-12-24 10:57:05 +0800

    Fix resolution bug

--- pinet/libraries/sasscompiler/precompiler/AutoResolution.php
+++ pinet/libraries/sasscompiler/precompiler/AutoResolution.php
@@ -49,7 +49,7 @@
 				}
 
 				$this->addBeforeResolution($compiler, $screen_width);
-				$compiler->suffix .= '@media screen and (min-width: '.$screen_width.'px) and (max-width: '.$next_screen_width.'px)  {'."\n";
+				$compiler->suffix .= '@media screen and (min-width: '.$screen_width.'px) {'."\n";
 				$compiler->suffix .= '$screen-width:'. $screen_width.';';
 			 	$compiler->suffix .= '$alias-width:'.$alias_width.';';
 			 	$this->addPrependResolution($compiler, $screen_width);
@@ -65,6 +65,21 @@
 				}
 				$this->addAppendResolution($compiler, $screen_width);
 				$compiler->suffix .= '}'."\n";
+				$compiler->suffix .= '@media screen and (min-width: '.$screen_width.'px) and (max-width: '.$next_screen_width.'px)  {'."\n";
+				$compiler->suffix .= '$screen-width:'. $screen_width.';';
+			 	$compiler->suffix .= '$alias-width:'.$alias_width.';';
+			 	$this->addPrependResolution($compiler, $screen_width);
+				foreach($compiler->sasses as $s) {
+					$s = str_replace('.scss', '', $s);
+					$basename = basename($s);
+					$name = str_replace('/', '_', $s);
+					if($basename == $name) {
+						$this->addSection($basename, $compiler, $screen_width.','.$alias_width);
+					}else {
+						$this->addSection($name, $compiler, $screen_width.','.$alias_width);
+					}
+				}
+				$compiler->suffix .= '}'."\n";
 				$this->addAfterResolution($compiler, $screen_width);
 			}
 			$this->addAfterResponsive($compiler);
@@ -72,6 +87,13 @@
 
 		protected function addConstruct($name, $compiler, $args) {
 			$the_name = 'responsive_'.$name;
+			if(strpos($compiler->content, $the_name) !== FALSE) {
+					$compiler->suffix .= "\t".'@include '.$the_name.'('.$args.');'."\n";
+			}
+		}
+
+		protected function addSection($name, $compiler, $args) {
+			$the_name = 'section_'.$name;
 			if(strpos($compiler->content, $the_name) !== FALSE) {
 					$compiler->suffix .= "\t".'@include '.$the_name.'('.$args.');'."\n";
 			}
