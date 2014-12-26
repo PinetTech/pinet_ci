b9cd174229ddcac72b922be38816958665c742f7
andy <andy.ling@pinet.co>
2014-12-26 16:42:57 +0800

    Merge with master

--- pinet/widgets/button/smarty/block.button_group.php
+++ pinet/widgets/button/smarty/block.button_group.php
@@ -3,6 +3,7 @@
 function smarty_block_button_group($params, $content = '', $template, &$repeat) {
 	if($repeat)
 		return;
+	$params['data-toggle'] = 'buttons';
 	$params['class'] = make_classes('btn-group' ,get_default($params, 'class', ''));
 	return create_tag('div', $params, array(), $content);
 }
