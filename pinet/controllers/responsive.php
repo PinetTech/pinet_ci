5f44861d34f374e51d80237da52c605c9a5fc5f1
Jack <guitarpoet@gmail.com>
2014-12-26 17:54:05 +0800

    Change responsive to CI_Controller for speed.

--- pinet/controllers/responsive.php
+++ pinet/controllers/responsive.php
@@ -1,6 +1,6 @@
 <?php defined("BASEPATH") or exit("No direct script access allowed");
 
-class Responsive extends Pinet_Controller {
+class Responsive extends CI_Controller {
 	public function __construct() {
 		parent::__construct();
 		$this->load->helper('image');
