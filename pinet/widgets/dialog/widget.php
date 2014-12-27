14a87601c4e9b3698b28785489b4a69e9067d873
andypinet <andy.ling@pinet.co>
2014-12-27 18:57:02 +0800

    Change listview style

--- pinet/widgets/dialog/widget.php
+++ pinet/widgets/dialog/widget.php
@@ -1,12 +1,11 @@
 <?php defined("BASEPATH") or exit("No direct script access allowed");
 
-class Listview_Widget extends Pinet_Widget {
+class Dialog_Widget extends Pinet_Widget {
 	public function __construct() {
 		parent::__construct();
 	}
 
 	public function init() {
 		parent::init();
-		$this->CI->load->library(array('listview'));
 	}
 }
