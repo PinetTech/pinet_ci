3f757645af7a6ffc7fa8688c1badc702050af8a3
Jack <guitarpoet@gmail.com>
2015-01-26 17:53:24 +0800

    Refactored the auto resolution precompiler.

--- pinet/Sass/AutoResolution.php
+++ pinet/Sass/AutoResolution.php
@@ -17,214 +17,141 @@ class AutoResolution extends \Clips\Libraries\Sass\SassPlugin {
 		   	$compiler);
 	}
 
-	public function suffix($compiler) {
+	protected function getSortedResolutions($compiler) {
 		$resolutions = $this->getResolutions($compiler);
+		$ret = $this->analyzeResolutions($resolutions);
+		usort($ret[0], function($a, $b) {
+			if($a['value'] == $b['value'])
+				return 0;
+			if($a['value'] > $b['value'])
+				return 1;
+			if($a['value'] < $b['value'])
+				return -1;
+			return false;
+		});
+		$result = $ret[0];
+		$resolutions = array();
+		$prev_value = null;
+		foreach($result as $r) {
+			$value = $r['value'];
 
-		$this->addBeforeResponsive($compiler);
-		foreach($resolutions as $k => $r) {
-			 if (is_numeric($k) && is_string($r) && !is_numeric($r) ) {
-			 	$screen_width = $k;
-			 	$alias_width =$r;
-			 }
-			 else if (is_numeric($k) && is_numeric($r)) {
-			 	$screen_width = $r;
-			 	$alias_width = 0;
-			 }
-			 else if (is_string($k) && !is_numeric($k)) {
-			 	$screen_width = $r;
-			 	$alias_width =$k;
-			 }
-			 else {
-			 	$screen_width = $k;
-			 	$alias_width =0;
-			 }
+			if($prev_value) {
+				$res = array(
+					'value' => $r,
+					'prev_value' => $prev_value,
+					'section' => true
+				);
 
-			$next_value = get_array_next($resolutions, $r);
-			if (is_string($next_value[1])) {
-				$next_screen_width = $next_value[0];
-			}
-			else {
-				$next_screen_width = $next_value[1];
-			}
+				$this->_process('before_section', $compiler->content, $res, $prev_value['value'].'_'.$value);
+				$this->_process('prepend_section', $compiler->content, $res, $prev_value['value'].'_'.$value);
+				$this->_process('append_section', $compiler->content, $res, $prev_value['value'].'_'.$value);
+				$this->_process('after_section', $compiler->content, $res, $prev_value['value'].'_'.$value);
+				$resolutions []= $res;
+			}	
 
-			if (!$next_screen_width) {
-				$next_screen_width = 2881;
-			}
+			$res = array(
+				'value' => $r,
+				'resolution' => true
+			);
 
-			$this->addBeforeResolution($compiler, $screen_width);
-			$compiler->suffix .= clips_out('media', array(
-				'media'=> '@media screen and (min-width: '.$screen_width.'px) {', 
-				'variables'=>array(array('variable'=>'$screen-width:'.$screen_width), array('variable'=>'$alias-width:'. $alias_width))
-			), false);
-		 	$this->addPrependResolution($compiler, $screen_width);
-			foreach($compiler->sasses as $s) {
-				$s = str_replace('.scss', '', $s);
-				$basename = basename($s);
-				$name = str_replace('/', '_', $s);
-				if($basename == $name) {
-					$this->addConstruct($basename, $compiler, $screen_width.','.$alias_width);
-				}else {
-					$names = explode('/', $s);
-					$index = array_search('scss', $names);
-					if ($index) {
-						array_splice($names, 0, $index+1);
-						$name = implode('_', $names);
-					}
-					$this->addConstruct($name, $compiler, $screen_width.','.$alias_width);
-				}
-			}
-			$this->addAppendResolution($compiler, $screen_width);
-			$compiler->suffix .= '}'."\n";
-			// $compiler->suffix .= '@media screen and (min-width: '.$screen_width.'px) and (max-width: '.($next_screen_width - 1).'px)  {'."\n";
-			// $compiler->suffix .= '$screen-width:'. $screen_width.';';
-			// $compiler->suffix .= '$alias-width:'.$alias_width.';';
-			// $compiler->suffix .= '$next-screen-width:'.$next_screen_width.';';
-			$compiler->suffix .= clips_out('media', array(
-				'media'=> '@media screen and (min-width: '.$screen_width.'px) and (max-width: '.($next_screen_width - 1).'px) {', 
-				'variables'=>array(
-					array('variable'=>'$screen-width:'.$screen_width), 
-					array('variable'=>'$alias-width:'. $alias_width)),
-					array('variable'=>'$next-screen-width:'.$next_screen_width)
-			), false);
-		 	// $this->addPrependResolution($compiler, $screen_width);
-			foreach($compiler->sasses as $s) {
-				$s = str_replace('.scss', '', $s);
-				$basename = basename($s);
-				$name = str_replace('/', '_', $s);
-				if($basename == $name) {
-					$this->addSection($basename, $compiler, $screen_width.','.$alias_width.','.$next_screen_width);
-				}else {
-					$names = explode('/', $s);
-					$index = array_search('scss', $names);
-					if ($index) {
-						array_splice($names, 0, $index+1);
-						$name = implode('_', $names);
-					}
-					$this->addSection($name, $compiler, $screen_width.','.$alias_width.','.$next_screen_width);
-				}
-			}
-			$compiler->suffix .= '}'."\n";
-			// $compiler->suffix .= '@media screen and (min-width: '.$screen_width.'px) and (max-width: '.($next_screen_width - 1).'px)  {'."\n";
-			// $compiler->suffix .= '$screen-width:'. $screen_width.';';
-			// $compiler->suffix .= '$alias-width:'.$alias_width.';';
-			// $compiler->suffix .= '$next-screen-width:'.$next_screen_width.';';
-			$compiler->suffix .= clips_out('media', array(
-				'media'=> '@media screen and (min-width: '.$screen_width.'px) and (max-width: '.($next_screen_width - 1).'px) {', 
-				'variables'=>array(
-					array('variable'=>'$screen-width:'.$screen_width), 
-					array('variable'=>'$alias-width:'. $alias_width)),
-					array('variable'=>'$next-screen-width:'.$next_screen_width)
-			), false);
-	 		$lasts = end($compiler->sasses);
-			$lasts = str_replace('.scss', '', $lasts);
-			$basename = basename($lasts);
-			$name = str_replace('/', '_', $lasts);
-			if($basename == $name) {
-				$this->addModule($basename, $compiler, $screen_width.','.$alias_width.','.$next_screen_width);
-			}else {
+			$this->_process('before_resolution', $compiler->content, $res, $value);
+			$this->_process('prepend_resolution', $compiler->content, $res, $value);
+			$this->_process('append_resolution', $compiler->content, $res, $value);
+			$this->_process('after_resolution', $compiler->content, $res, $value);
+
+			$resolutions []= $res;
+			$prev_value = $r;
+		}
+		return $resolutions;
+	}
+
+	protected function _process($name, $content, $res, $value = null) {
+		$n = $name;
+		if($value) {
+			$n = $name.'_'.$value;
+		}
+		if(strpos($content, $n) !== FALSE) {
+			$res[$name] = $n;
+			return true;
+		}
+		return false;
+	}
+
+	protected function getSasses($sasses, $content) {
+		$ret = array();
+		foreach($sasses as $s) {
+			// Get the script name
+			$s = str_replace('.scss', '', $s);
+			$basename = basename($s);
+			$name = str_replace('/', '_', $s);
+			if($basename != $name) {
 				$names = explode('/', $s);
 				$index = array_search('scss', $names);
 				if ($index) {
 					array_splice($names, 0, $index+1);
 					$name = implode('_', $names);
 				}
-				$this->addModule($name, $compiler, $screen_width.','.$alias_width.','.$next_screen_width);
 			}
-			$compiler->suffix .= '}'."\n";
-			// $this->addAfterResolution($compiler, $screen_width);
+			
+			if(strpos($content, 'responsive_'.$name) !== FALSE) {
+				$ret []= 'responsive_'.$name;
+			}
+
+			if(strpos($content, 'section_'.$name) !== FALSE) {
+				$ret []= 'section_'.$name;
+			}
+
+			if(strpos($content, 'module_'.$name) !== FALSE) {
+				$ret []= 'module_'.$name;
+			}
 		}
-		$this->addAfterResponsive($compiler);
+		return $ret;
 	}
 
-	protected function addConstruct($name, $compiler, $args) {
-		$the_name = 'responsive_'.$name;
-		if(strpos($compiler->content, $the_name) !== FALSE) {
-				$compiler->suffix .= "\t".'@include '.$the_name.'('.$args.');'."\n";
-		}
+	public function suffix($compiler) {
+		$data = array(
+			'before' => strpos($compiler->content, 'before_responsive') !== FALSE,
+			'after' => strpos($compiler->content, 'after_responsive') !== FALSE,
+			'resolutions' => $this->getSortedResolutions($compiler),
+			'sasses' => $this->getSasses($compiler->sasses, $compiler->content)
+		);
+
+		$compiler->suffix .= clips_out('media', $data, false);
 	}
 
-	protected function addModule($name, $compiler, $args) {
-		$the_name = 'module_'.$name;
-		if(strpos($compiler->content, $the_name) !== FALSE) {
-				$compiler->suffix .= "\t".'@include '.$the_name.'('.$args.');'."\n";
-		}
-	}
+	protected function analyzeResolutions($resolutions) {
+		$result = array();
+		$min = 0;
+		$max = 0;
+		foreach($resolutions as $k => $v) {
+			$arr = null;
+			if(is_string($k)) {
+				$arr = array('alias' => $k, 'value' => $v);
+			}
+			else {
+				if(is_string($v)) {
+					$arr = array('alias' => $v, 'value' => $k);
+				}
+				else {
+					$arr = array('alias' => 0, 'value' => $v);
+				}
+			}
+			$result []= $arr;
+			if($min > $arr['value'])
+				$min = $arr['value'];
 
-	protected function addSection($name, $compiler, $args) {
-		$the_name = 'section_'.$name;
-		if(strpos($compiler->content, $the_name) !== FALSE) {
-				$compiler->suffix .= "\t".'@include '.$the_name.'('.$args.');'."\n";
+			if($max < $arr['value'])
+				$max = $arr['value'];
 		}
-	}
-
-	protected function addPrependResolution($compiler, $res) {
-		$the_name = 'prepend_resolution_'.$res;
-		if(strpos($compiler->content, $the_name) !== FALSE) {
-			$compiler->suffix .= "\t".'@include '.$the_name.'();'."\n";
-		}
-	}
-
-	protected function addAppendResolution($compiler, $res) {
-		$the_name = 'append_resolution_'.$res;
-		if(strpos($compiler->content, $the_name) !== FALSE) {
-			$compiler->suffix .= "\t".'@include '.$the_name.'();'."\n";
-		}
-	}
-
-	protected function addBeforeResolution($compiler, $res) {
-		$the_name = 'before_resolution_'.$res;
-		if(strpos($compiler->content, $the_name) !== FALSE) {
-			$compiler->suffix .= "\t".'@include '.$the_name.'();'."\n";
-		}
-	}
-
-	protected function addAfterResolution($compiler, $res) {
-		$the_name = 'after_resolution_'.$res;
-		if(strpos($compiler->content, $the_name) !== FALSE) {
-			$compiler->suffix .= "\t".'@include '.$the_name.'();'."\n";
-		}
-	}
-
-	protected function addBeforeResponsive($compiler) {
-		$the_name = 'before_responsive';
-		if(strpos($compiler->content, $the_name) !== FALSE) {
-			$compiler->suffix .= "\t".'@include '.$the_name.'();'."\n";
-		}
-	}
-
-	protected function addAfterResponsive($compiler) {
-		$the_name = 'after_responsive';
-		if(strpos($compiler->content, $the_name) !== FALSE) {
-			$compiler->suffix .= "\t".'@include '.$the_name.'();'."\n";
-		}
+		return array($result, $min, $max);
 	}
 
 	protected function appendVariables($resolutions, $compiler) {
 		if (is_array($resolutions)) {
-			$result = array();
-			$max = 0;
-			$min = 0;
-
-			foreach($resolutions as $k => $v) {
-				$arr = null;
-				if(is_string($k)) {
-					$arr = array('alias' => $k, 'value' => $v);
-				}
-				else {
-					if(is_string($v)) {
-						$arr = array('alias' => $v, 'value' => $k);
-					}
-					else {
-						$arr = array('alias' => 0, 'value' => $v);
-					}
-				}
-				$result []= $arr;
-				if($min > $arr['value'])
-					$min = $arr['value'];
-
-				if($max < $arr['value'])
-					$max = $arr['value'];
-			}
+			$ret = $this->analyzeResolutions($resolutions);
+			$result = $ret[0];
+			$max = $ret[1];
+			$min = $ret[2];
 
 			$str = 'string://$min-screen-width: {{min}};
 	$max-screen-width: {{max}};
@@ -251,7 +178,7 @@ class AutoResolution extends \Clips\Libraries\Sass\SassPlugin {
 				'min' => $min,
 				'max' => $max,
 				'resolutions' => $result
-			));
+			), false);
 		}
 	}
 
--- pinet/templates/media.tpl
+++ pinet/templates/media.tpl
@@ -1,4 +1,45 @@
-{{media}} 
-{{#variables}}
-	{{variable}};
-{{/variables}}
+{{#before}}
+@include before_responsive();
+{{/before}}
+{{#resolutions}}
+{{#resolution}}
+{{#before_resolution}}
+	{{before_resolution}}();
+{{/before_resolution}}
+@media screen and (min-width: {{value.value}}px) {
+	$screen-width: {{value.value}};
+	$alias-width: {{value.alias}};
+	{{#prepend_resolution}}
+		{{prepend_resolution}}({{value.value}});
+	{{/prepend_resolution}}
+	{{#sasses}}
+	@include {{.}}({{value.value}}, {{value.alias}});
+	{{/sasses}}
+	{{#append_resolution}}
+		{{append_resolution}}({{value.value}});
+	{{/append_resolution}}
+}
+{{#after_resolution}}
+	{{after_resolution}}();
+{{/after_resolution}}
+{{/resolution}}
+{{#section}}
+{{#before_section}}
+	{{before_section}}();
+{{/before_section}}
+@media screen and (min-width: {{prev_value.value}}px) and (max-width: {{value.value}}px) {
+	$screen-width: {{prev_value.value}};
+	$alias-width: {{prev_value.alias}};
+	$next-screen-width: {{value.value}};
+	{{#sasses}}
+	@include {{.}}({{prev_value.value}}, {{value.alias}}, {{value.value}});
+	{{/sasses}}
+}
+{{#after_section}}
+	{{after_section}}();
+{{/after_section}}
+{{/section}}
+{{/resolutions}}
+{{#after}}
+@include after_responsive();
+{{/after}}
